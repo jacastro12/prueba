5581d414ff81a07ae34f0b08056bc2f29f3a2ffb
unknown <jonacastro95@gmail.com>
2019-08-13 19:34:24 -0500

    cambios en categoria

--- appmacas/models.py
+++ appmacas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Categorias(models.Model):
     id_categorias = models.AutoField(primary_key=True)
-    # imagen = models.ImageField(max_length=150, upload_to='categorias')
+    imagen = models.ImageField(max_length=150, upload_to='categorias')
     # imagen_crop = ImageRatioField('imagen', '75x75', size_warning=True, free_crop=True)
     # imagen_crop_index = ImageRatioField('imagen', '400x133', size_warning=True)
     nombre = models.CharField(max_length=150)
